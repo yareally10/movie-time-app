7e112caff66e4719ac4a0173afcb9f31e519fb9a
Lin Yi <lin.yi@omnicell.com>
2021-03-12 13:22:33 -0500

    add xhr call to details page; add hooks and update page to handle xhr

--- src/screens/DetailsScreen.js
+++ src/screens/DetailsScreen.js
@@ -1,19 +1,48 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Button, View, Text, StyleSheet } from "react-native";
 
 const DetailsScreen = ({ navigation, route }) => {
   console.log(route);
   const { movie } = route.params;
+  const xhr = new XMLHttpRequest();
+  const serverUrl = "http://www.omdbapi.com/";
+  const apiKey = "xxxxxx"; // replace with real api key
+  const movieTitle = encodeURI(movie.title.replace(" ", "+"));
+  const movieYear = encodeURI(movie.release);
+  const [movieDetails, setMovieDetails] = useState(null);
+
+  useEffect(() => {
+    xhr.open(
+      "GET",
+      `${serverUrl}?apikey=${apiKey}&t=${movieTitle}&y=${movieYear}`
+    );
+    xhr.send();
+    xhr.onload = () => {
+      if (xhr.status === 200) {
+        let response = JSON.parse(xhr.response);
+        setMovieDetails(response);
+        console.log(response);
+      } else {
+        console.log(
+          `HTTP Request Failed.\nStatus: ${xhr.status}\nResponse: ${xhr.response}`
+        );
+      }
+    };
+  }, []);
+
   return (
     <View style={styles.mainView}>
-      <Text style={{ fontSize: 20 }}>
-        {movie.title} ({movie.release})
-      </Text>
-      <Text style={{ fontSize: 100 }}>{movie.screenNumber}</Text>
+      <Text>{movieDetails && movieDetails.Title}</Text>
+      <Text>{movieDetails && movieDetails.Released}</Text>
+      <Text>{movieDetails && movieDetails.Plot}</Text>
       <Button
         title="Go to Image"
         onPress={() => {
-          navigation.navigate("BigImageView");
+          if (movieDetails) {
+            navigation.navigate("BigImageView", {
+              uri: movieDetails.Poster,
+            });
+          }
         }}
       />
       <Button
