3386b5cea61c06d476cb693eb0bff37d2485d966
Lin Yi <lin.yi@omnicell.com>
2021-03-12 13:22:45 -0500

    add poster image to image screen

--- src/screens/ImageScreen.js
+++ src/screens/ImageScreen.js
@@ -1,14 +1,21 @@
 import React from "react";
-import { Button, View, Text, StyleSheet } from "react-native";
+import { Button, View, Text, StyleSheet, Image } from "react-native";
 
-const ImageScreen = () => {
+const ImageScreen = (props) => {
+  console.log(props.route.params.uri);
   return (
     <View style={styles.mainView}>
       <Text>Image Screen</Text>
+      <Image
+        style={styles.imageView}
+        source={{
+          uri: props.route.params.uri,
+        }}
+      />
       <Button
         title="Go Home"
         onPress={() => {
-          console.log("Go to Details");
+          props.navigation.popToTop();
         }}
       />
     </View>
@@ -20,6 +27,10 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
+  },
+  imageView: {
+    width: 400,
+    height: 640,
   },
 });
 
